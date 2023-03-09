e0d886a899e014a2924bc2cadffe131ce4d2b7b2
Vivek Suryavanshi <vivek@Viveks-MacBook-Air.local>
2023-03-09 06:14:15 +0530

    code updated

--- src/test/java/testcases/test00.java
+++ src/test/java/testcases/test00.java
@@ -5,23 +5,19 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import base.BaseClass;
-import dataProvider.CustomDataProvider;
-import pageObjects.HomePage;
-import pageObjects.LoginPage;
-
-public class test00 extends BaseClass {
+public class test00 {
 WebDriver driver;
 	
 	@BeforeMethod
 	public void browserLaunch()
 	{
 		
-		driver = new ChromeDriver();
+		driver = new SafariDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 	}
